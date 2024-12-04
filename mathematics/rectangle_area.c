b8caa8d85882199a2003ce81365f641a6e57ba88
arihantsingh04 <arihants2004@gmail.com>
2024-12-04 14:21:19 +0530

    added all sucessfully

---
+++ mathematics/rectangle_area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int r=0;int area=0;
+    printf("Enter radius of the circle: ");
+    scanf("%d",&r);
+    area=2*(r*r);
+    printf("Area of the Rectangle will be: %d sq. unit",area);
+    
+    return 0;
+}
